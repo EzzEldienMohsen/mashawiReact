9ecdda56c6526918f1c758c4dc081df4917fd1a0
Ezz dghedy <ezzmohsend@gmail.com>
2024-08-05 00:27:03 +0200

    about second section re design

--- src/components/AboutWhy.tsx
+++ src/components/AboutWhy.tsx
@@ -3,25 +3,28 @@ import { whyArray } from '../assets';
 import { useTranslation } from 'react-i18next';
 import whyImg from '../assets/Image.png';
 
-const AboutWhy:React.FC = () => {
+const AboutWhy: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <div className="flex flex-col lg:flex-row-reverse justify-center items-center w-full lg:bg-white lg:justify-between  ">
-      <img src={whyImg} alt="alt" className="w-full lg:w-1/2 md:h-1/4 lg:h-auto" />
+    <div className="flex flex-col lg:flex-row-reverse justify-center items-center w-full lg:bg-white lg:justify-between">
+      <img
+        src={whyImg}
+        alt="alt"
+        className="w-full lg:w-1/2 md:h-1/4 lg:h-auto"
+      />
       <div className="font-abdo flex flex-col justify-center items-center gap-y-6 px-8 lg:px-20 py-4 w-full lg:w-1/2">
         <h1 className="text-black font-bold font-abdo text-2xl lg:text-3xl">
           {t('aboutWhySectionTitle')}
         </h1>
-        <ul className="w-full flex flex-col   items-start justify-center gap-y-4">
+        <ul className="w-full flex flex-col items-start justify-center gap-y-4">
           {whyArray.map((why) => (
             <li
               key={why.id}
-              className="flex flex-row items-start justify-start gap-x-4 lg:gap-x-6  w-full"
+              className="flex flex-row items-start justify-start gap-x-4 lg:gap-x-6 w-full"
             >
-              <img src={why.src} alt="logo" className="w-10 aspect-square" />
-
+              <img src={why.src} alt="logo" className="w-8 " />
               <div className="flex flex-col gap-y-1">
-                <p className="font-semibold  text-black text-sm md:text-md lg:text-lg">
+                <p className="font-semibold text-black text-sm md:text-md lg:text-lg">
                   {t(why.title)}
                 </p>
                 <p className="text-black text-xs md:text-sm lg:text-md">
