87ba0932c24007fff3fbda2c0c2cbad014c58f92
Ezz dghedy <ezzmohsend@gmail.com>
2024-08-07 09:36:29 +0200

    some modal  adjustments

--- src/subSubSubComponents/AddOn.tsx
+++ src/subSubSubComponents/AddOn.tsx
@@ -28,8 +28,8 @@ const AddOn: React.FC<AddOnProps> = ({
   };
 
   return (
-    <div className="bg-bgColor flex justify-between w-full items-center py-4 px-4 my-2 rounded-lg shadow-md">
-      <div className="flex gap-x-4 w-1/3 items-center">
+    <div className="bg-bgColor flex justify-between w-full items-center py-2 px-4 my-1 rounded-lg shadow-md">
+      <div className="flex gap-x-3 w-1/3 items-center">
         <img
           src={addOn.img}
           alt="alt"
@@ -37,7 +37,7 @@ const AddOn: React.FC<AddOnProps> = ({
         />
         <h1 className=" font-semibold text-xs font-abdo">{t(addOn.name)}</h1>
       </div>
-      <div className="flex gap-x-4 items-center justify-start text-thin font-abdo text-xs w-1/4">
+      <div className="flex gap-x-2 items-center justify-start text-thin font-abdo text-xs w-1/4">
         <label className="flex items-center space-x-2">
           <input
             type="checkbox"
