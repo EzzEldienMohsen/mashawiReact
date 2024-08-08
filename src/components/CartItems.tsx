eb9753fd68d22ced3064183c2dd7fdf7ecd0b03a
Ezz dghedy <ezzmohsend@gmail.com>
2024-08-08 11:00:06 +0200

    cart adjustments

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -4,8 +4,10 @@ import { removeItem } from '../features/cart/cartSlice';
 import { CartCard } from '../subComponents';
 import { AppDispatch, RootState, useTypedSelector } from '../store';
 import { CartItem } from '../assets/types';
+import { useTranslation } from 'react-i18next';
 
 const CartItems: React.FC = () => {
+  const { t } = useTranslation();
   const dispatch: AppDispatch = useDispatch();
   const removeItemsFromCart = (prod: CartItem) => {
     dispatch(removeItem(prod));
@@ -13,6 +15,15 @@ const CartItems: React.FC = () => {
   const { cartItems } = useTypedSelector(
     (state: RootState) => state.theMashawiCart
   );
+  if (cartItems.length === 0) {
+    return (
+      <div className=" w-full px-8 lg:px-20">
+        <div className="flex justify-center items-center bg-white text-center my-8 py-5 rounded-2xl shadow-md ">
+          <h1 className="text-black font-abdo font-light">{t('emptyCart')}</h1>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="my-16 w-full flex flex-col justify-center items-center px-8 lg:px-20">
       {cartItems.map((item) => {
--- src/components/DrawerCartItems.tsx
+++ src/components/DrawerCartItems.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { RootState, useTypedSelector } from '../store';
 import { DrawerCartCard } from '../subComponents';
+import { useTranslation } from 'react-i18next';
 
 const DrawerCartItems: React.FC = () => {
+  const { t } = useTranslation();
   const { cartItems } = useTypedSelector(
     (state: RootState) => state.theMashawiCart
   );
-
+  if (cartItems.length === 0) {
+    return (
+      <div className=" w-full px-8 lg:px-20">
+        <div className="flex justify-center items-center bg-white text-center my-8 py-5 rounded-2xl shadow-md ">
+          <h1 className="text-black font-abdo font-light">{t('emptyCart')}</h1>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col justify-center items-center gap-y-1 z-40">
       {cartItems.map((item) => {
