c837e289207f0c649e04c47911c1a1afcb41c267
Ezz dghedy <ezzmohsend@gmail.com>
2024-08-08 10:24:15 +0200

    category empty list

--- src/subComponents/MenuCard.tsx
+++ src/subComponents/MenuCard.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Card } from '../subSubComponents';
 import { MealData } from '../assets/types';
+import { useTranslation } from 'react-i18next';
 
 const MenuCard: React.FC<{ data: MealData }> = ({ data }) => {
+  const { t } = useTranslation();
+  if (data.length === 0) {
+    return (
+      <div className="flex justify-center items-center bg-white text-center w-full my-8 py-5 rounded-2xl shadow-md ">
+        <h1 className="text-black font-abdo font-light">{t('emptyCat')}</h1>
+      </div>
+    );
+  }
   return (
     <div className="my-8 flex flex-col justify-center items-center gap-y-5 md:flex-row md:grid md:grid-cols-2 lg:flex lg:flex-row lg:justify-start lg:gap-x-5  lg:flex-wrap w-full">
       {data.map((data) => {
