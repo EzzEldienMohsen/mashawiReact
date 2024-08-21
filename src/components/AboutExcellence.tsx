b3e6930a564db5e3ad3594d99ba4d4e96096fb23
Ezz dghedy <ezzmohsend@gmail.com>
2024-08-21 13:13:38 +0200

    adjustment

--- src/components/AboutExcellence.tsx
+++ src/components/AboutExcellence.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 const AboutExcellence: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <div className=" flex flex-col justify-center items-center my-12 px-8 lg:px-20">
+    <div className=" flex flex-col justify-center items-center 2xl:gap-y-10 my-12 px-8 lg:px-20">
       <SectionTitle title={t('aboutExcellenceSectionTitle')} />
       <div className="flex flex-col justify-start items-start gap-y-6 lg:flex-row lg:justify-evenly lg:items-center w-full lg:flex-wrap">
         {excellence.map((card) => {
@@ -18,13 +18,15 @@ const AboutExcellence: React.FC = () => {
               <img
                 src={card.src}
                 alt="alt"
-                className="h-32 w-20 md:h-24 lg:w-24  lg:h-32"
+                className="h-32 w-20 md:h-24 lg:w-24  lg:h-32 2xl:w-36 2xl:h-40"
               />
               <div className="flex flex-col justify-between items-start gap-y-5 font-abdo">
-                <h1 className="font-bold text-md md:text-lg ">
+                <h1 className="font-bold text-md md:text-lg 2xl:text-3xl">
                   {t(card.title)}
                 </h1>
-                <p className="text-xs md:text-sm lg:text-sm">{t(card.text)}</p>
+                <p className="text-xs md:text-sm lg:text-sm 2xl:text-2xl">
+                  {t(card.text)}
+                </p>
               </div>
             </div>
           );
--- src/components/SidePageHero.tsx
+++ src/components/SidePageHero.tsx
@@ -17,11 +17,11 @@ const SidePageHero: React.FC<SidePageHeroProps> = ({
         isJob ? 'pt-2' : 'py-4'
       } md:mb-12 lg:mt-16 mb-20`}
     >
-      <div className="flex flex-col md:text-start justify-center items-center md:justify-start md:items-start lg:w-2/5">
-        <h1 className="mb-4 text-lg md:text-2xl lg:text-4xl my-1 md:text-start font-bold text-[#CD0B09]">
+      <div className="flex flex-col md:text-start justify-center items-center md:justify-start 2xl:gap-y-8 md:items-start lg:w-2/5">
+        <h1 className="mb-4 text-lg md:text-2xl lg:text-4xl 2xl:text-6xl my-1 md:text-start font-bold text-[#CD0B09]">
           {primaryText}
         </h1>
-        <p className="text-white text-sm md:text-md text-center md:text-start lg:text-lg my-1">
+        <p className="text-white text-sm md:text-md 2xl:text-2xl text-center md:text-start lg:text-lg my-1">
           {secondaryText}
         </p>
       </div>
@@ -31,7 +31,7 @@ const SidePageHero: React.FC<SidePageHeroProps> = ({
         className={`${
           isJob
             ? 'md:-mt-16 lg:-mt-28'
-            : 'mt-2 -mb-24 md:-my-14 lg:-my-20 2xl:-my-28'
+            : 'mt-2 -mb-24 md:-my-14 lg:-my-20 2xl:-my-36'
         }  w-1/2 md:w-1/4 lg:w-1/3 aspect-square `}
       />
     </div>
