38a8a2224a358afffef423ffb095fabb1b4b8daf
Ezz dghedy <ezzmohsend@gmail.com>
2024-08-22 08:18:22 +0200

    morning adj

--- src/subSubSubComponents/ProductDetails.tsx
+++ src/subSubSubComponents/ProductDetails.tsx
@@ -35,12 +35,11 @@ const ProductDetails: React.FC<ProductProps> = ({
       </div>
       <div className="flex justify-between w-full flex-row items-center my-2">
         <button
-          className="btn bg-newRed text-white flex justify-between items-center px-4 rounded-full py-2 2xl:w-2/5 2xl:py-4 2xl:px-5"
+          className="btn bg-newRed text-white flex justify-between items-center px-4 rounded-full py-4 2xl:w-2/5 2xl:py-2 2xl:px-4 2xl:min-h-[40px] 2xl:h-auto"
           onClick={handleAddToCart}
-          style={{ height: 'auto', minHeight: '40px' }}
         >
-          <img src={addOrder} alt="alt" className="2xl:w-1/4" />
-          <span className="font-abdo 2xl:text-3xl">{t('addOrder')}</span>
+          <img src={addOrder} alt="alt" className="2xl:w-[22%]" />
+          <span className="font-abdo 2xl:text-xl">{t('addOrder')}</span>
         </button>
         <AmountInput amount={amount} setAmount={setAmount} />
       </div>
